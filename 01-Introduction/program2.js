006f35c72f20a6f13eb320f32bb9ff15381b6d49
veeresh8055 <visesync@gmail.com>
2026-02-17 20:19:24 +0530

    First Program

---
+++ 01-Introduction/program2.js
@@ -0,0 +1,12 @@
+let name = 'veeresh';
+let password = 'veeresh@1234'
+let phoneNo = 6558542145
+
+console.log(name)
+console.log(password)
+console.log(phoneNo)
+
+// output
+// veeresh
+// veeresh@1234
+// 6558542145
