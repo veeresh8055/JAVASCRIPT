271de556d4ebae310c19f86da411a40ff064e058
veeresh8055 <visesync@gmail.com>
2026-03-13 21:42:20 +0530

    Express

--- NodeJs/class02/server.js
+++ NodeJs/class02/server.js
@@ -1,6 +1,19 @@
 const express = require('express')
 const app = express()
 
+app.get('/',(req,res)=>{
+  res.send("Welcome to Main Page")
+})
+
+app.get('/home',(req,res)=>{
+  res.send("Welcome to Home Page")
+})
+
+
+app.get('/about',(req,res)=>{
+  res.send("Welcome to About Page")
+})
+
 app.listen(3000,()=>{
     console.log('Server started Expresssss...')
 })
