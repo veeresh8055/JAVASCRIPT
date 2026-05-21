dce000cd7d9ce56824961ebd7e4e40b751fccc22
veeresh8055 <visesync@gmail.com>
2026-05-21 10:52:59 +0530

    Image Caption Generator

--- 02-NodeJs/class16-CaptionGenerator/src/service/ai.service.js
+++ 02-NodeJs/class16-CaptionGenerator/src/service/ai.service.js
@@ -20,19 +20,20 @@ async function generateCaption(base64ImageFile) {
     model: "gemini-3.5-flash",
     contents: contents,
     config:{
-      systemInstruction:`Generate a catchy social media caption for the uploaded image.
-
-Requirements:
-- Caption length must be between 15 and 20 words.
-- Use engaging and natural language.
-- Include 2–4 relevant emojis.
-- Add 3–5 trending and relevant hashtags at the end.
-- Make it suitable for both Twitter/X and Instagram.
-- Tone should be modern, energetic, and attention-grabbing.
-- Avoid generic captions and repetition.
-- Match the caption to the mood, colors, and subject of the image.
-
-Output only the caption. `
+      systemInstruction:`
+        Generate a catchy social media caption for the uploaded image.
+          Requirements:
+         - Caption length must be between 15 and 20 words.
+         - Use engaging and natural language.
+         - Include 2–4 relevant emojis.
+         - Add 3–5 trending and relevant hashtags at the end.
+         - Make it suitable for both Twitter/X and Instagram.
+         - Tone should be modern, energetic, and attention-grabbing.
+         - Avoid generic captions and repetition.
+         - Match the caption to the mood, colors, and subject of the image.
+         
+         Output only the caption. 
+      `
     }
   });
   return  response.text
