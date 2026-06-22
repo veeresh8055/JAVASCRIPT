d3f8b951f5efe82cf6ded9dcf1697f9ac340422f
veeresh8055 <visesync@gmail.com>
2026-06-22 14:04:28 +0530

    app

--- 02-NodeJs/NodeClass/url/app.js
+++ 02-NodeJs/NodeClass/url/app.js
@@ -4,6 +4,24 @@ let urlLink = 'http://localhost:3000/login?username=veeresh&password=1234'
 
 let urlData = new URL(urlLink)
 
-console.log(urlData)
-const {search}= urlData
-console.log(search)
+// console.log(urlData) // details about the url 
+
+console.log(urlData.hostname)
+
+console.log(urlData.searchParams) // it is a  map structure 
+// use map methods to get the values
+console.log(urlData.searchParams.get('username'))
+console.log(urlData.searchParams.get('password'))
+
+
+
+
+
+
+// ~~~~~~~ example for map structure ~~~~~~~
+// const userMap = new Map([
+//   ['username', 'veeresh'],
+//   ['password', '1234']
+// ]);
+
+// console.log(userMap.get('username'))
