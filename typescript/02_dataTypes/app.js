689be4b08f928c60c0d58f5e143fe6f09245bce4
veeresh8055 <visesync@gmail.com>
2026-08-28 17:35:13 +0530

    Any , Unknown , null , undefined , Never

--- typescript/02_dataTypes/app.js
+++ typescript/02_dataTypes/app.js
@@ -5,6 +5,7 @@
 // Tuples
 // Enum 
 // Any , unknown  , Void , Null , Undefined , Never 
+// --> difference between any and unknown 
 Object.defineProperty(exports, "__esModule", { value: true });
 // primitives 
 let number = 10;
@@ -17,4 +18,42 @@ let num = [1, 2, 3]; // Only for Numbers it stores
 console.log(num);
 let names = ["hello", "Typescript"];
 console.log(names);
+// tuples : fixed two diffreent data type in an array 
+let users = ["age", 22];
+// console.log(  users.map(el=> console.log(el ) )  )
+// Enums -> Enumerations 
+var userRoles;
+(function (userRoles) {
+    userRoles["ADMIN"] = "admin";
+    userRoles["USER"] = "user";
+    userRoles["GUEST"] = "guest";
+})(userRoles || (userRoles = {}));
+console.log(userRoles);
+console.log(userRoles.ADMIN);
+//Any -> when we didnt set the datatypes of a variable by defualt it is Any -> we have to set the datatype of that variable -> in future we can change the the data type of a variable of it like we did in js 
+let a; // type : any
+a = 10; // now type is Number 
+a = "hello"; // now the type is String 
+// -> we avoid to set a datatype of a vaiable into any , because we can change the type in runtime 
+// unknow 
+let b;
+b = 20;
+b = "hello";
+if (typeof b === "string") {
+    let res = b.endsWith("o");
+    console.log(res);
+}
+// Void --> if a function doesnt return anything set that into void 
+function greet() {
+    console.log("Helo the function return nothing so its 'void' ");
+}
+greet();
+//null 
+let owner = null; //-> type : any 
+let newOwner;
+// newOwner = 10  --> when we set a variable with null in future we cannot change its value we have to add only a null for this the solution is we use a union
+//union 
+let newOwn; // the dataype is string or null 
+newOwn = null;
+newOwn = "veeresh";
 //# sourceMappingURL=app.js.map
--- typescript/02_dataTypes/app.ts
+++ typescript/02_dataTypes/app.ts
@@ -4,6 +4,7 @@
 // Tuples
 // Enum 
 // Any , unknown  , Void , Null , Undefined , Never 
+// --> difference between any and unknown 
 
 
 // primitives 
@@ -23,4 +24,51 @@ let names : String[] = ["hello" , "Typescript"]
 console.log(names)
 
 
+// tuples : fixed two diffreent data type in an array 
 
+let users : [String , Number ] = ["age" ,22 ]
+// console.log(  users.map(el=> console.log(el ) )  )
+
+// Enums -> Enumerations 
+enum  userRoles{
+    ADMIN = "admin",
+    USER = "user",
+    GUEST = "guest"
+}
+console.log(userRoles)
+console.log(userRoles.ADMIN)
+
+
+//Any -> when we didnt set the datatypes of a variable by defualt it is Any -> we have to set the datatype of that variable -> in future we can change the the data type of a variable of it like we did in js 
+let a ;  // type : any
+a = 10 ;  // now type is Number 
+a = "hello" ;  // now the type is String 
+// -> we avoid to set a datatype of a vaiable into any , because we can change the type in runtime 
+
+
+// unknow 
+let b:unknown ;
+b = 20 
+b="hello"
+
+if(typeof b === "string" ){
+     let res =   b.endsWith("o")
+     console.log(res)
+}
+
+
+// Void --> if a function doesnt return anything set that into void 
+function greet():void {
+    console.log("Helo the function return nothing so its 'void' ")
+}
+greet()
+
+//null 
+let owner = null //-> type : any 
+let newOwner : null ;
+// newOwner = 10  --> when we set a variable with null in future we cannot change its value we have to add only a null for this the solution is we use a union
+
+//union 
+let newOwn : String | null ;  // the dataype is string or null 
+newOwn = null ;
+newOwn = "veeresh"
